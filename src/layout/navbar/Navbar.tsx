fd8a7bae24654621d213585b152ca574147634d5
Pike Havelka <havelka.pvl@seznam.cz>
2023-12-04 19:12:22 +0100

    repaired loading bar in navbar

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -11,74 +11,71 @@ export const Navbar = () => {
   }
 
   return (
-    <>
-      <nav className="navbar-main">
-        <div className="container flex-wrapper">
-          <div className="navbar-logo">
-            <a href="/" className="navbar-logo-link">
-              <span className="logo-remodel">Remodel<span className="logo-to">TO</span></span>
-              <span className="logo-motto">Vy sníte, my stavíme</span>
-            </a>
-          </div>
-
-          <ul className={showMenu ? "show" : "hide"}>
-            <li>
-              <a href="/#o-nas">O nás</a>
-            </li>
-
-            <li>
-              <a href="/#sluzby">Služby</a>
-            </li>
-
-            <li>
-              <a href="/#reference">Reference</a>
-            </li>
-
-            <li>
-              <a href="#kontakt">Kontakt</a>
-            </li>
-          </ul>
-
-          <button
-            className="navbar-hamburger-menu-btn"
-            onClick={hamburgerMenuHandle}
-            aria-controls="primární navigace"
-            aria-expanded={showMenu}
-            aria-label="Tlačitko zobrazující hlavní navigaci stránky"
-          >
-            <svg
-              className="navbar-hamburger-menu-svg"
-              stroke="var(--button-color)"
-              fill="none"
-              viewBox="-10 -10 120 120"
-              width="30"
-              height="25"
-            >
-              <path
-                className="line"
-                strokeWidth="8"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m 20 40 h 60 a 1 1 0 0 1 0 20 h -60 a 1 1 0 0 1 0 -40 h 30 v 70"
-              >
-              </path>
-            </svg>
-          </button>
+    <nav className="navbar-main">
+      <div className="container flex-wrapper">
+        <div className="navbar-logo">
+          <a href="/" className="navbar-logo-link">
+            <span className="logo-remodel">Remodel<span className="logo-to">TO</span></span>
+            <span className="logo-motto">Vy sníte, my stavíme</span>
+          </a>
         </div>
-      </nav>
+
+        <ul className={showMenu ? "show" : "hide"}>
+          <li>
+            <a href="/#o-nas">O nás</a>
+          </li>
+
+          <li>
+            <a href="/#sluzby">Služby</a>
+          </li>
+
+          <li>
+            <a href="/#reference">Reference</a>
+          </li>
+
+          <li>
+            <a href="#kontakt">Kontakt</a>
+          </li>
+        </ul>
+
+        <button
+          className="navbar-hamburger-menu-btn"
+          onClick={hamburgerMenuHandle}
+          aria-controls="primární navigace"
+          aria-expanded={showMenu}
+          aria-label="Tlačitko zobrazující hlavní navigaci stránky"
+        >
+          <svg
+            className="navbar-hamburger-menu-svg"
+            stroke="var(--button-color)"
+            fill="none"
+            viewBox="-10 -10 120 120"
+            width="30"
+            height="25"
+          >
+            <path
+              className="line"
+              strokeWidth="8"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m 20 40 h 60 a 1 1 0 0 1 0 20 h -60 a 1 1 0 0 1 0 -40 h 30 v 70"
+            >
+            </path>
+          </svg>
+        </button>
+      </div>
 
       <motion.div
         style={{
           scaleX: scrollYProgress,
-          height: 15,
           background: "var(--decoration-color)",
           transformOrigin: "0",
           position: "fixed",
           minWidth: "100%",
-          top: "2.9rem",
+          height: 15,
           zIndex: "10"
         }}>
       </motion.div>
-    </>
+    </nav>
   )
 }
