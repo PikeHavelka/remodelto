8b0246987e76a4e688fdeae7065652fb12271229
Pike Havelka <havelka.pvl@seznam.cz>
2023-12-31 16:09:55 +0100

    Error page is now more visible

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,10 +1,23 @@
 import "./errorPage.scss"
+import { useEffect, useRef } from "react"
 
 export const ErrorPage = () => {
+  const errorPageRef = useRef(null)
+
+  useEffect(() => {
+    const errorPageCurrent = errorPageRef.current as HTMLHeadingElement | null
+
+    if(errorPageCurrent) errorPageCurrent.scrollIntoView({ block: "center"})
+
+    return () => {
+      if (errorPageCurrent) errorPageCurrent.scrollIntoView(false)
+    }
+  })
+
   return (
     <section className="error-page container">
       <div className="error-page-text-container">
-        <h1 className="error-page-title">Stránka nenalezena | chyba 404</h1>
+        <h1 className="error-page-title" ref={errorPageRef}>Stránka nenalezena | chyba 404</h1>
 
         <p className="error-page-text">Nepodařilo se nám najít požadovanou stránku. Prosím zkontrolujte zadanou url adresu.</p>
       </div>
