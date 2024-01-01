1999f0755269badde5d38133b267190ce59ddcc6
Pike Havelka <havelka.pvl@seznam.cz>
2024-01-01 17:56:07 +0100

    added imges logos and create cooperation component

--- src/layout/cooperation/Cooperation.tsx
+++ src/layout/cooperation/Cooperation.tsx
@@ -1,9 +1,20 @@
 import "./cooperation.scss"
+import dataCooperationLogos from "../../data/dataCooperationLogos"
 
 export const Cooperation = () => {
   return (
-    <section id="spoluprace">
+    <section id="spoluprace" className="cooperation">
+      <h2 className="interest-subtitle">Naše spolupráce</h2>
 
+      <div className="cooperation-logos-container container">
+        {dataCooperationLogos.map((oneLogo, index) => {
+          return (
+            <a href={oneLogo.url} key={index} aria-label={oneLogo.ariaLabel}>
+              <img src={oneLogo.img} alt="Logo firmy" className="logo" />
+            </a>
+          )
+        })}
+      </div>
     </section>
   )
 }
